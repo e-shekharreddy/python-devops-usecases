b5fc408095e2d616ee1f01c0843928eae1d710d0
e-shekharreddy <shekharreddy.eanala.com>
2026-09-15 00:18:31 +0530

    python basics

--- terraform-python-ec2/provider.tf
+++ terraform-python-ec2/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
     backend "s3" {
         bucket         = "remote-state-esr-dev"
-        key            = "roboshop-dev-vpc"
+        key            = "python-usecases"
         region         = "us-east-1"
         encrypt        = true
         use_lockfile   = true
